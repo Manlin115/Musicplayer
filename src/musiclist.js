99da98749347ca7dfb37b6ca1821232e61b3d8a2
Manlin115 <160145580+Manlin115@users.noreply.github.com>
2024-04-08 22:27:51 +0530

    Add files via upload

---
+++ src/musiclist.js
@@ -0,0 +1,58 @@
+import {writable} from "svelte/store";
+
+export const musicList = writable([
+    {
+        image: "img7.jpg",
+        audio: "rasathi.mp3",
+        name: "Rasathi_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img2.jpg",
+        audio: "vj_bgm.mp3",
+        name: "Gilli_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img3.jpg",
+        audio: "Krish.mp3",
+        name: "Krish_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img4.jpg",
+        audio: "sara sara.mp3",
+        name: "Sara_sara_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img5.jpg",
+        audio: "empty.mp3",
+        name: "Empty_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img6.jpg",
+        audio: "anthaaruvipool.mp3",
+        name: "Anthaaruvi_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img1.jpg",
+        audio: "mobile.mp3",
+        name: "Airtel_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img8.jpg",
+        audio: "swar_bgm.mp3",
+        name: "Swar_Bgm",
+        artist: "Artist: Manlin"
+    },
+    {
+        image: "img9.jpg",
+        audio: "ennoduneeirrunthal.mp3",
+        name: "I-movie_Bgm",
+        artist: "Artist: Manlin"
+    }, 
+]);
